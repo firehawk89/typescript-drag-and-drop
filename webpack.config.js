60cfb6990961cba4ced3369678a6135eee396552
Bochkovskyi <unundertale1@ukr.net>
2023-07-18 13:33:20 +0300

    adjust sourcemaps and build

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  devtools: "inline-source-map",
+  devtool: "inline-source-map",
   module: {
     rules: [{ test: /\.ts$/, use: "ts-loader", exclude: "/node_modules/" }],
   },
